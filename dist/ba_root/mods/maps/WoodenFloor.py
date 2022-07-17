2f0773c2c98e6de11ad1d3b11ba671e707925753
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2022-07-17 13:36:13 +0530

    added more games and maps

---
+++ dist/ba_root/mods/maps/WoodenFloor.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import ba,_ba
+from bastd.gameutils import SharedObjects
+from bastd.actor.playerspaz import PlayerSpaz
+if TYPE_CHECKING:
+    from typing import Any, List, Dict
+
+
+class WoodenFloor(ba.Map):
+    """Stadium map for football games."""
+    from bastd.mapdata import football_stadium as defs
+    defs.points['spawn1'] = (-12.03866341, 0.02275111462, 0.0) + (0.5, 1.0, 4.0)
+    defs.points['spawn2'] = (12.823107149, 0.01092306765, 0.0) + (0.5, 1.0, 4.0)
+    name = 'Wooden Floor'
+
+    @classmethod
+    def get_play_types(cls) -> list[str]:
+        """Return valid play types for this map."""
+        return ['melee', 'football', 'team_flag', 'keep_away']
+
+    @classmethod
+    def get_preview_texture_name(cls) -> str:
+        return 'footballStadiumPreview'
+
+    @classmethod
+    def on_preload(cls) -> Any:
+        data: dict[str, Any] = {
+           
+            'model_bg': ba.getmodel('doomShroomBG'),
+            'bg_vr_fill_model': ba.getmodel('natureBackgroundVRFill'),
+            'collide_model': ba.getcollidemodel('bridgitLevelCollide'),
+            'tex': ba.gettexture('bridgitLevelColor'),
+            'model_bg_tex': ba.gettexture('doomShroomBGColor'),
+            'collide_bg': ba.getcollidemodel('natureBackgroundCollide'),
+            'railing_collide_model':
+                (ba.getcollidemodel('bridgitLevelRailingCollide')),
+            'bg_material': ba.Material()
+        }
+        data['bg_material'].add_actions(actions=('modify_part_collision',
+                                                 'friction', 10.0))
+        return data
+
+    def __init__(self) -> None:
+        super().__init__()
+        shared = SharedObjects.get()
+        self.background = ba.newnode(
+            'terrain',
+            attrs={
+                'model': self.preloaddata['model_bg'],
+                'lighting': False,
+                'background': True,
+                'color_texture': self.preloaddata['model_bg_tex']
+            })
+        ba.newnode('terrain',
+                   attrs={
+                       'model': self.preloaddata['bg_vr_fill_model'],
+                       'lighting': False,
+                       'vr_only': True,
+                       'background': True,
+                       'color_texture': self.preloaddata['model_bg_tex']
+                   })
+        gnode = ba.getactivity().globalsnode
+        gnode.tint = (1.3, 1.2, 1.0)
+        gnode.ambient_color = (1.3, 1.2, 1.0)
+        gnode.vignette_outer = (0.57, 0.57, 0.57)
+        gnode.vignette_inner = (0.9, 0.9, 0.9)
+        gnode.vr_camera_offset = (0, -0.8, -1.1)
+        gnode.vr_near_clip = 0.5
+        # self.map_extend()
+
+    def is_point_near_edge(self,
+                           point: ba.Vec3,
+                           running: bool = False) -> bool:
+        box_position = self.defs.boxes['edge_box'][0:3]
+        box_scale = self.defs.boxes['edge_box'][6:9]
+        xpos = (point.x - box_position[0]) / box_scale[0]
+        zpos = (point.z - box_position[2]) / box_scale[2]
+        return xpos < -0.5 or xpos > 0.5 or zpos < -0.5 or zpos > 0.5
+    
+      
+    def _handle_player_collide(self):
+        try:
+            player = ba.getcollision().opposingnode.getdelegate(
+                PlayerSpaz, True)
+        except ba.NotFoundError:
+            return
+        
+        
+        if player.is_alive():
+            player.shatter(True)
+
+
+        
+
+ba._map.register_map(WoodenFloor)
