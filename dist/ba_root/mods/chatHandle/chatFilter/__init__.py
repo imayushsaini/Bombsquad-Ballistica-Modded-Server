2e6f150bf872e86ec79ea610970725da1ec0103b
Ayush Saini <36878972+imayushsaini@users.noreply.github.com>
2023-08-15 17:36:36 +0530

    deleting different case folder names

--- dist/ba_root/mods/chatHandle/chatFilter/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Common bits of functionality shared between all efro projects.
-
-Things in here should be hardened, highly type-safe, and well-covered by unit
-tests since they are widely used in live client and server code.
-
-license : MIT, see LICENSE for more details.
-"""
